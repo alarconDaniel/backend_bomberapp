773df684d9770bc9f2c7610ece122a9c744b22d3
Philipoh20 <luisfsuarezrojas@gmail.com>
2025-09-03 18:36:17 -0500

    arreglos funcion borrar usuario
    Presentaba un error al borrar porque la columna de estadisticas usuarios no permitia el borrado asi que se le añadio el metodo de cascada para eso

--- src/models/estadistica-usuario/estadistica-usuario.ts
+++ src/models/estadistica-usuario/estadistica-usuario.ts
@@ -6,6 +6,8 @@ import {
   OneToOne,
   PrimaryGeneratedColumn,
   Index,
+  ManyToOne,
+  RelationId,
 } from 'typeorm';
 import { Usuario } from '../usuario/usuario';
 
@@ -15,12 +17,7 @@ export class EstadisticaUsuario {
   @PrimaryGeneratedColumn({ name: 'cod_estadistica', type: 'int' })
   codEstadistica!: number;
 
-  // Parte 2 de PK (ref a usuarios.cod_usuario)
-  // Nota: no definimos @Column separado; el JoinColumn crea la columna 'cod_usuario'.
-  @OneToOne(() => Usuario, (u) => u.estadisticas, { eager: true })
-  @JoinColumn({ name: 'cod_usuario', referencedColumnName: 'codUsuario' })
-  @Index('fk_estadisticas_usuario_usuarios1') // coincide con tu FK (nombre orientativo)
-  usuario!: Usuario;
+ 
 
   @Column({ name: 'monedas_estadistica', type: 'int', default: 0 })
   monedas!: number;
@@ -36,4 +33,20 @@ export class EstadisticaUsuario {
       'La experiencia total con la que cuenta el usuario; niveles se calculan en el backend',
   })
   xp!: number;
+   // Parte 2 de PK (ref a usuarios.cod_usuario)
+  // Nota: no definimos @Column separado; el JoinColumn crea la columna 'cod_usuario'.
+  @OneToOne(() => Usuario, (u) => u.estadisticas, {
+    eager: true,         // opcional
+    nullable: false,     // si vas a usar SET NULL, cámbialo a true
+    onDelete: 'CASCADE', // o 'SET NULL' si no quieres borrado en cascada
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'cod_usuario', referencedColumnName: 'codUsuario' })
+  @Index('fk_estadisticas_usuario_usuarios1') // nombre orientativo
+  usuario!: Usuario;
+
+    // Acceder al id sin traer el objeto completo
+    @RelationId((e: EstadisticaUsuario) => e.usuario)
+    codUsuario!: number;
 }
+
