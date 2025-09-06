81dde5b71bfe9fdba96c5e3161d4f9fdb93ac997
Philipoh20 <luisfsuarezrojas@gmail.com>
2025-09-06 10:44:47 -0500

    backend limpio después de migración

--- src/modules/public/archivo/archivo.module.ts
+++ src/modules/public/archivo/archivo.module.ts
@@ -4,14 +4,11 @@ import { Archivo } from '../../../models/archivo/archivo';
 import { Usuario } from '../../../models/usuario/usuario';
 import { ArchivoService } from './archivo.service';
 
-// ⬇️ importa el módulo que expone GoogleDriveOAuthService
-// ajusta el nombre/ruta si en tu proyecto se llama distinto
-import { GoogleDriveOAuthModule } from '../google-token/google-token.module';
+
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Archivo, Usuario]), 
-    forwardRef(() => GoogleDriveOAuthModule),          
+    TypeOrmModule.forFeature([Archivo, Usuario]),          
   ],
   providers: [ArchivoService],
   exports: [ArchivoService],
